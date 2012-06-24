c1b46136527116758ca572ba9f1246c8efa151ac
Sayak Banerjee <sayakb@kde.org>
2012-06-24 12:55:11 +0530

    Default to active program if only one is active

--- modules/mod_home.php
+++ modules/mod_home.php
@@ -5,6 +5,20 @@
 * @copyright (c) 2012 KDE. All rights reserved.
 */
 
+// Get the IDs of active programs
+$sql = "SELECT id FROM {$db->prefix}programs " .
+       "WHERE is_active = 1";
+$results = $db->query($sql);
+
+// Take to the program home directly if there's only one program
+if (count($results) == 1)
+{
+    $id  = $results[0]['id'];
+    $url = "?q=program_home&prg={$id}";
+
+    $core->redirect($url);
+}
+
 // Set the module data
 $module_title = $lang->get('home');
 $module_data = $skin->output('tpl_home');;
--- modules/mod_view_programs.php
+++ modules/mod_view_programs.php
@@ -24,43 +24,58 @@ $limit = "LIMIT {$limit_start}, {$config->per_page}";
 $program_data = $db->query($data_sql . $filter . $limit);
 $program_count = $db->query($count_sql . $filter, true);
 
-// Generate a list
-$programs_list = '';
-
-foreach ($program_data as $row)
+// If only one program is active, directly take the user to the destination
+if ($program_count['count'] == 1 && count($program_data) == 1)
 {
+    $row = $program_data[0];
+
     // Generate the program URL
-    $url  = !empty($return_url) ? "?q=view_projects&amp;prg={$row['id']}&amp;a={$return_url}"
-                                : "?q=program_home&amp;prg={$row['id']}";
-    
-    // Assign data for program
+    $url  = !empty($return_url) ? "?q=view_projects&prg={$row['id']}&a={$return_url}"
+                                : "?q=program_home&prg={$row['id']}";
+
+    // Redirect to the URL
+    $core->redirect($url);
+}
+else
+{
+    // Generate a list
+    $programs_list = '';
+
+    foreach ($program_data as $row)
+    {
+        // Generate the program URL
+        $url  = !empty($return_url) ? "?q=view_projects&amp;prg={$row['id']}&amp;a={$return_url}"
+                                    : "?q=program_home&amp;prg={$row['id']}";
+
+        // Assign data for program
+        $skin->assign(array(
+            'program_url'         => $url,
+            'program_title'       => htmlspecialchars($row['title']),
+            'program_description' => htmlspecialchars($row['description']),
+        ));
+
+        $programs_list .= $skin->output('tpl_view_programs_item');
+    }
+
+    // Determine the page title
+    $programs_title = !empty($return_url) ? $lang->get('select_program') : $lang->get('view_active_progms');
+
+    // Get the pagination
+    $pagination = $skin->pagination($program_count['count'], $page);
+
+    // Assign final skin data
     $skin->assign(array(
-        'program_url'         => $url,
-        'program_title'       => htmlspecialchars($row['title']),
-        'program_description' => htmlspecialchars($row['description']),
+        'programs_title'    => $programs_title,
+        'programs_list'     => $programs_list,
+        'list_pages'        => $pagination,
+        'notice_visibility' => $skin->visibility(count($program_data) == 0),
+        'list_visibility'   => $skin->visibility(count($program_data) > 0),
+        'pages_visibility'  => $skin->visibility($program_count['count'] > $config->per_page),
     ));
 
-    $programs_list .= $skin->output('tpl_view_programs_item');
+    // Output the module
+    $module_title = $programs_title;
+    $module_data = $skin->output('tpl_view_programs');
 }
-
-// Determine the page title
-$programs_title = !empty($return_url) ? $lang->get('select_program') : $lang->get('view_active_progms');
-
-// Get the pagination
-$pagination = $skin->pagination($program_count['count'], $page);
-
-// Assign final skin data
-$skin->assign(array(
-    'programs_title'    => $programs_title,
-    'programs_list'     => $programs_list,
-    'list_pages'        => $pagination,
-    'notice_visibility' => $skin->visibility(count($program_data) == 0),
-    'list_visibility'   => $skin->visibility(count($program_data) > 0),
-    'pages_visibility'  => $skin->visibility($program_count['count'] > $config->per_page),
-));
-
-// Output the module
-$module_title = $programs_title;
-$module_data = $skin->output('tpl_view_programs');
 
 ?>
